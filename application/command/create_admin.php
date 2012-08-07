90248c4bf547ee579eb74ed1336b76b2b798ffa4
Evan Byrne <evantbyrne@gmail.com>
2012-08-07 11:43:28 -0400

    Added create_admin command

---
+++ application/command/create_admin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Command;
+
+/**
+ * Create Admin Command
+ * @author Evan Byrne
+ */
+class Create_Admin {
+	
+	/**
+	 * Run
+	 */
+	public function run() {
+		
+		$user = new \Model\User();
+		
+		echo 'Email: ';
+		ob_flush();
+		$user->email = trim(fgets(STDIN));
+		
+		echo 'Password: ';
+		ob_flush();
+		$user->password = trim(fgets(STDIN));
+		
+		if(is_array($user->valid())) {
+			
+			echo "Error: email and password are required\n";
+		
+		} else {
+			
+			$user->set_password($user->password);
+			$user->type = \Model\User::level('admin');
+			$user->save();
+			echo "Success: created admin user with email: {$user->email}\n";
+			
+		}
+		
+	}
+	
+}
