5248a95aa97175518d7672fc1f3297c20d684415
Evan Byrne <evantbyrne@gmail.com>
2012-08-10 18:31:43 -0400

    Fixed create_model and create_controller commands

--- application/command/create_controller.php
+++ application/command/create_controller.php
@@ -26,7 +26,7 @@ class Create_Controller {
 			} else {
 				
 				$f = fopen($path, 'w');
-				fwrite($f, "<?php\n\nnamespace Controller;\n\nclass $con extends Base {\n\n}");
+				fwrite($f, "<?php\n\nnamespace Controller;\n\nclass $con extends \Core\Controller\Base {\n\n}");
 				fclose($f);
 				
 			}
