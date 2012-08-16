f3b72231a5ae9c9d24e3073b378e17a98df4bbca
Evan Byrne <evantbyrne@gmail.com>
2012-08-16 14:05:23 -0400

    Updated \Column\Image thumbnail style

--- application/template/field/image.php
+++ application/template/field/image.php
@@ -6,7 +6,13 @@
 	<?php $location = $model->{$field . '_path'}(); ?>
 
 	<p><small>Choose new file to replace uploaded one:</small></p>
-	<p><a href="<?php e(url($location)); ?>"><img style="max-width:300px" src="<?php e(url($location)); ?>" /></a></p>
+	<ul class="thumbnails">
+		<li style="margin:0">
+			<a href="<?php e(url($location)); ?>" class="thumbnail">
+				<img style="max-width:300px" src="<?php e(url($location)); ?>" />
+			</a>
+		</li>
+	</ul>
 
 <?php endif; ?>
 
