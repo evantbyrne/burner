d36d84a375b4d6b9e40829e6940bbd0a1d265e8b
Evan Byrne <evantbyrne@gmail.com>
2012-08-17 10:49:13 -0400

    Added alter_add command

---
+++ application/command/alter_add.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Command;
+
+/**
+ * Alter Add Command
+ * @author Evan Byrne
+ */
+class Alter_Add {
+	
+	/**
+	 * Run
+	 * @param string Model
+	 * @param string Field
+	 */
+	public function run($model, $field) {
+			
+		echo "Altering table to add: $model.$field\n";
+		$model_class = "\\Model\\$model";
+		$table = $model_class::table();
+		
+		$instance = new $model_class();
+		$column = $instance->get_schema_column($field) or die("Error: Column '$field' not found in model.\n");
+		
+		\Core\DB::connection()
+			->alter_table($table)
+			->add($column->column())
+			->execute();
+		
+	}
+	
+}
--- application/model/user.php
+++ application/model/user.php
@@ -7,4 +7,15 @@ namespace Model;
  */
 class User extends \Core\Model\User {
 	
+	/**
+	 * @inheritdoc
+	 */
+	public function __construct() {
+		
+		parent::__construct();
+		$this->schema(new \Column\Image('avatar', array('dir' => 'static/user/avatar')));
+		$this->admin('avatar');
+		
+	}
+	
 }
