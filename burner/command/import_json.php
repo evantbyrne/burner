dd70fef149358a6a98429720071fc2e14203e663
Evan Byrne <evantbyrne@gmail.com>
2012-09-04 13:02:49 -0400

    Added import_json command

---
+++ burner/command/import_json.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Core\Command;
+
+/**
+ * Import JSON Command
+ * @author Evan Byrne
+ */
+class Import_Json {
+	
+	/**
+	 * Help
+	 */
+	public function help() {
+
+		echo "\nimport_json <model> <input_file>\n\n";
+		echo "Description:\n";
+		echo "\tImports JSON file to MySQL table for model.\n\n";
+
+	}
+
+	/**
+	 * Run
+	 * @param string Model
+	 * @param string File
+	 */
+	public function run($model, $filename) {
+			
+		echo "Importing contents of '$filename' to '$model' table...\n";
+		
+		// Get data
+		$data = file_get_contents($filename);
+		$json = json_decode($data, true);
+
+		// Insert data into table
+		$model_class = "\\Model\\$model";
+		$table = $model_class::table();
+		$n = 0;
+
+		foreach($json as $row) {
+
+			$n++;
+			$insert = $model_class::insert();
+			foreach($row as $column => $value) {
+
+				$insert->value($column, $value);
+
+			}
+
+			$insert->execute();
+
+		}
+
+		echo "$n rows inserted.\n\n";
+		
+	}
+	
+}
