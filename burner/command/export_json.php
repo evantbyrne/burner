e6fa9e37f784c97fa9a45a08e21d5deea7d1cdd6
Evan Byrne <evantbyrne@gmail.com>
2012-09-04 10:50:48 -0400

    Added export_json command

---
+++ burner/command/export_json.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Core\Command;
+
+/**
+ * Export JSON Command
+ * @author Evan Byrne
+ */
+class Export_Json {
+	
+	/**
+	 * Help
+	 */
+	public function help() {
+
+		echo "\nexport_json <model> <output_file>\n\n";
+		echo "Description:\n";
+		echo "\tExports contents of MySQL table to a file.\n\n";
+
+	}
+
+	/**
+	 * Run
+	 * @param string Model
+	 * @param string Field
+	 */
+	public function run($model, $filename) {
+			
+		echo "Exporting contents of '$model' to $filename...\n\n";
+		$model_class = "\\Model\\$model";
+		$table = $model_class::table();
+		
+		$res = \Core\DB::connection()->select($table)->order_desc('id')->fetch();
+		
+		$fh = fopen($filename, 'w');
+		fwrite($fh, json_encode($res));
+		fclose($fh);
+		
+	}
+	
+}
